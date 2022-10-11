3cd5100daf9f128ed49a3678143b3c8fa7a1f4a0
andreibarboni <andrei.barboni01@e-uvt.ro>
2022-10-11 14:50:47 +0300

    My initial commit

---
+++ src/MyBook/Book.java
@@ -0,0 +1,32 @@
+package MyBook;
+public class Book {
+    public String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String print() {
+        return "Titlu ='" + title;
+    }
+
+    public void createNewParagraph(String paragraph){
+        System.out.println(paragraph);
+    }
+
+    public void createNewImage(String img){
+        System.out.println(img);
+    }
+
+    public void createNewTable(String table){
+        System.out.println(table);
+    }
+}
